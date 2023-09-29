273deedc438cadbdfbf76f19f3cd4de55b1c04e3
Pratyush Tiwari <pratyushtiwarimj@gmail.com>
2023-09-29 10:39:10 +0530

    Modal class added

--- PratyushDoodlesApp/models/Shop/ShopModel.py
+++ PratyushDoodlesApp/models/Shop/ShopModel.py
@@ -1,8 +1,38 @@
 from flask import Flask
 from ... import db
 
-class Item(db.Model):
-    id = db. Column (db. Integer, primary_key=True)
+class CartItem(db.Model):
+    __tablename__ = 'cart_items'
 
-    def __repr_ (self) :
-        return f"Item ('{self .id}')"
+    id = db.Column(db.Integer, primary_key=True)
+    item_name = db.Column(db.String(255), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    item_description = db.Column(db.Text, nullable=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+
+    # Define a one-to-many relationship between CartItem and ItemPicture
+    pictures = db.relationship('ItemPicture', backref='cart_item', lazy=True)
+
+    def __init__(self, item_name, quantity, item_description=None, user=None):
+        self.item_name = item_name
+        self.quantity = quantity
+        self.item_description = item_description
+        self.user = user  # Associate the cart item with a user
+
+    def __repr__(self):
+        return f'<CartItem {self.item_name}>'
+
+class ItemPicture(db.Model):
+    __tablename__ = 'item_pictures'
+
+    id = db.Column(db.Integer, primary_key=True)
+    picture_url = db.Column(db.String(255), nullable=False)
+    cart_item_id = db.Column(db.Integer, db.ForeignKey('cart_items.id'), nullable=False)  # ForeignKey to CartItem
+
+    def __init__(self, picture_url, item=None):
+        self.picture_url = picture_url
+        self.cart_item = cart_item  # Associate the picture with a cart item
+
+    def __repr__(self):
+        return f'<ItemPicture {self.picture_url}>'
--- PratyushDoodlesApp/models/User/UserModel.py
+++ PratyushDoodlesApp/models/User/UserModel.py
@@ -1,14 +1,40 @@
 from flask import Flask
 from ... import db
 
-class UserProfile(db.Model):
-    id = db.Column(db. Integer, primary_key=True)
-    
-    def __repr_ (self) :
-        return f"UserProfile ('{self .id}')"
+class User(db.Model):
+    __tablename__ = 'users'
 
-class ContactInfo(db.Model):
-    id = db.Column(db. Integer, primary_key=True) 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    mobile_number = db.Column(db.String(15), unique=True, nullable=False)
+    email = db.Column(db.String(255), nullable=True)
+    address = db.Column(db.String(255), nullable=True)
 
-    def __repr_ (self) :
-        return f"ContactInfo ('{self .id}')"
+    # Define a one-to-many relationship between User and CartItem
+    cart_items = db.relationship('CartItem', backref='user', lazy=True)
+
+    def __init__(self, name, mobile_number, address=None, email=None):
+        self.name = name
+        self.mobile_number = mobile_number
+        self.address = address
+        self.email = email
+
+    def __repr__(self):
+        return f'<User {self.name}>'
+
+    def add_item_to_cart(self, item_name, quantity, item_description=None, item_picture_urls=None):
+        # Create a new cart item and associate it with the user
+        cart_item = CartItem(item_name=item_name, quantity=quantity, item_description=item_description, user=self)
+
+        # Associate item pictures with the cart item
+        if item_picture_urls:
+            for picture_url in item_picture_urls:
+                item_picture = ItemPicture(picture_url=picture_url, item=cart_item)
+                db.session.add(item_picture)
+
+        db.session.add(cart_item)
+        db.session.commit()
+
+    def get_cart_items(self):
+        # Retrieve and return all cart items associated with the user
+        return self.cart_items
