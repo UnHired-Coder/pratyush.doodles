9c75b82af2d5282a74a8fa39b3bc0284f70e2405
Pratyush Tiwari <pratyushtiwarimj@gmail.com>
2023-10-02 12:01:54 +0530

    Add product working

--- PratyushDoodlesApp/business/shop.py
+++ PratyushDoodlesApp/business/shop.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, redirect, url_for, flash, Blueprint
 from .. import app
-from ..forms.AddItemForm import AddItemForm
+from ..forms.AddItemForm import ProductForm
 from ..models.UserModel import User
 from .util import *
-from ..models.ProductModel import Product
+from ..models.ProductModel import Product, ProductImage
 from .. import db
 
 shop_bp = Blueprint('shop', __name__)
@@ -19,5 +19,44 @@ def shop():
     }
 
     return render_template('shop.html', data = data)
+
+
+#Admin actions
+@shop_bp.route('/add_product', methods=['GET', 'POST'])
+def add_product():
+    form = ProductForm()
+    print(form.validate())
+    if form.validate_on_submit():
+        product_name = form.name.data
+        product_description = form.description.data
+        product_price = form.price.data
+        product_stock_quantity = form.stock_quantity.data
+
+        # Get the list of image URLs
+        image_urls = [url for url in form.images.data.split(',')]
+
+        # Now you have the product data and the list of image URLs
+
+        # Redirect to a success page or perform further actions
+        product = Product(name = product_name, description = product_description, price = product_price, stock_quantity=product_stock_quantity)
+        db.session.add(product)
+        db.session.flush()
+
+        for image_url in image_urls:
+            image = ProductImage(picture_url = image_url, product_id = product.id)
+            db.session.add(image)
+            db.session.flush()
+
+        db.session.commit()    
+
+        print(image_urls)
+    else:
+        print(form.errors)
+
+        # images = ProductImage()
+        
+
+    return render_template('add_item.html', form=form)
+   
 
 app.register_blueprint(shop_bp)
--- PratyushDoodlesApp/forms/AddItemForm.py
+++ PratyushDoodlesApp/forms/AddItemForm.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, DecimalField, IntegerField, FieldList, FormField
+from wtforms.validators import DataRequired, NumberRange, URL, Optional
 
-class AddItemForm(FlaskForm):
-    item_name = StringField('Item Name', validators=[DataRequired()])
-    quantity = IntegerField('Quantity', validators=[DataRequired()])
-    item_description = TextAreaField('Item Description')
-    item_picture_urls = StringField('Item Picture URLs')
+class ProductForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[Optional()])
+    price = IntegerField('Price', validators=[DataRequired(), NumberRange(min=0)])
+    stock_quantity = IntegerField('Stock Quantity', validators=[DataRequired(), NumberRange(min=0)])
+    images = StringField('Image URL', validators=[DataRequired()])
