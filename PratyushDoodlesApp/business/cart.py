87165910921976a406c08f3cc4ac370c239c97af
Pratyush Tiwari <pratyushtiwarimj@gmail.com>
2023-10-02 16:15:05 +0530

    Cart items shown

--- PratyushDoodlesApp/business/cart.py
+++ PratyushDoodlesApp/business/cart.py
@@ -1,14 +1,31 @@
 from flask import render_template
 from flask import Blueprint
 from .. import app
+from ..models.UserModel import Cart
+from ..models.ProductModel import Product
+from .util import *
+
+
 
 cart_bp = Blueprint('cart', __name__)
 
 @cart_bp.route('/cart')
 def cart():
+    user = get_current_user()
+    cart = Cart.query.filter_by(user_id=1).first()
+
+    cart_items = []
+
+    for item in cart.cart_items:
+        product = Product.query.filter_by(id = item.product_id).first()
+        cart_items.append({
+            'product':product,
+            'quantity':item.quantity
+        })
 
     data = {
-        'title': "no name"
+        'user': user,
+        'cart_items': cart_items
     }
 
     return render_template('cart.html', data=data)
