0ce995b9226108d60cfe1e15a93c02e440ac00ec
Pratyush Tiwari <pratyushtiwarimj@gmail.com>
2023-10-11 19:50:30 +0530

    Razor pay payment integration

--- PratyushDoodlesApp/__init__.py
+++ PratyushDoodlesApp/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
+import razorpay
 
 
 def create_app():
@@ -8,13 +9,16 @@ def create_app():
 
     app.secret_key = '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'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-    
+    app.config['RAZORPAY_KEY_ID'] = 'rzp_test_arOA73zUdfEWbl'
+    app.config['RAZORPAY_KEY_SECRET'] = 'C05oOgLdPwM9LaiAAbAOveeX'
+
     return app
 
 
 app = create_app()
 socketio = SocketIO(app, async_mode = None)
 db = SQLAlchemy(app)
+razorpayClient = razorpay.Client(auth = (app.config['RAZORPAY_KEY_ID'], app.config['RAZORPAY_KEY_SECRET']))
 
 from PratyushDoodlesApp.business.cart import *
 from PratyushDoodlesApp.business.auth import *
--- PratyushDoodlesApp/business/checkout.py
+++ PratyushDoodlesApp/business/checkout.py
@@ -1,6 +1,5 @@
-from flask import render_template
-from flask import Blueprint
-from .. import app
+from flask import render_template, Blueprint, jsonify
+from .. import app, razorpayClient
 from .util import *
 from .cart import getCartData
 
@@ -13,5 +12,22 @@ def checkout():
     user = get_current_user()
     data = getCartData()
     return render_template('checkout.html', data=data)
+
+@checkout_bp.route('/initiate_payment', methods=['GET', 'POST'])
+def initiate_payment():
+    amount = 1000  # The amount to be paid in paise (e.g., 1000 paise = ₹10)
+    currency = 'INR'  # Currency code (e.g., INR for Indian Rupees)
+
+    payment_data = {
+        'amount': amount,
+        'currency': currency,
+    }
+
+    # Create a Razorpay order
+    order = razorpayClient.order.create(payment_data)
+
+    # Return the order ID to the client-side for payment initiation
+    return jsonify({'order_id': order['id']})
+
 
 app.register_blueprint(checkout_bp)
