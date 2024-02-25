f230974248aa1b83e8938f59d6d4a7962c294bc3
Pratyush Tiwari <pratyushtiwarimj@gmail.com>
2024-02-25 19:21:09 +0530

    Testing

--- PratyushDoodlesApp/business/shop.py
+++ PratyushDoodlesApp/business/shop.py
@@ -76,8 +76,8 @@ def get_products():
 
     try:
         product_in_this_category = Product.query.filter_by(product_category = category).order_by(Product.id.desc()).offset(start_id).limit(12).all()  
-        if len(product_in_this_category) != 0:
-            shuffle(product_in_this_category) 
+        # if len(product_in_this_category) != 0:
+            # shuffle(product_in_this_category) 
             
     except Exception as e:
         product_in_this_category = []
